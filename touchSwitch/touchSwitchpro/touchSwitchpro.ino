8599cc9bdfeeefe0147456f1a062216be59248b0
Quang <nguyentrungquang102@gmail.com>
2021-11-01 01:04:56 +0700

    updated for PWM module

--- touchSwitch/touchSwitchpro/touchSwitchpro.ino
+++ touchSwitch/touchSwitchpro/touchSwitchpro.ino
@@ -1,11 +1,19 @@
 #include <ShiftRegister74HC595.h>
 #include <ADCTouch.h>
-#define refer_val 500
-#define threshold 200
+#define refer_val 10
+#define threshold 60
+
+#define PWM1_PIN 8
+#define PWM2_PIN 9
+#define MAX_DUTY 50
+
+const int ledPin =  LED_BUILTIN;  // the number of the LED pin
+int ledState = LOW;             // ledState used to set the LED
 
 int *ref;//reference values to remove offset
+
 // parameters: <number of shift registers> (data pin, clock pin, latch pin)
-ShiftRegister74HC595<3> sr(0, 1, 2);
+ShiftRegister74HC595<3> sr(5, 7, 6);
   
 static int* init_touchSwitch()
 {
@@ -21,9 +29,83 @@ static int* init_touchSwitch()
   return &ref[0];
 }
 
+void toggle_led(int idx)
+{
+  static int flag_toggle=0;
+
+  if(flag_toggle != 0)
+  {
+    sr.set(idx, HIGH); // set single pin HIGH
+    flag_toggle = 0;
+  }
+  else
+  {
+    sr.set(idx, LOW); // set single pin LOW
+    flag_toggle = 1;
+  }
+  
+}
+
+void init_timer()
+{
+  // initialize Timer1
+  noInterrupts(); // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = 400;  //31250/2/1000;  // compare match register 16MHz/256/2Hz
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= ((1 << CS10));    // 256 prescaler
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts
+}
+
+uint8_t cnt=0,cnt_on=0,cnt_off=0;
+int pwm1_state = LOW;
+int pwm2_state = LOW;
+uint8_t pwm_duty1=10;
+uint8_t pwm_duty2=5;
+
+ISR(TIMER1_COMPA_vect) // timer compare interrupt service routine
+{ 
+  if(cnt < MAX_DUTY)
+  {
+    cnt++;
+  } 
+  else
+  {
+      cnt = 0;
+  }
+
+  if(cnt<pwm_duty1)
+  {
+    pwm1_state = HIGH;
+  } 
+  else 
+  {
+    pwm1_state = LOW;
+  }
+
+  if(cnt<pwm_duty2)
+  {
+    pwm2_state = HIGH;
+  } 
+  else 
+  {
+    pwm2_state = LOW;
+  }
+  
+  // digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  digitalWrite(PWM1_PIN, pwm1_state);
+  digitalWrite(PWM2_PIN, pwm2_state);
+
+}
+
 void init_led_module()
 {
   sr.setAllLow(); // set all pins LOW
+  //uint8_t pinValues[] = {0xAA, 0xAA, 0xAA}; 
+  //sr.setAll(pinValues); 
 }
 
 void set_led_on(int idx)
@@ -50,10 +132,12 @@ void set_all_led_on(int idx)
 
 void setup() 
 {
-    // No pins to setup, pins can still be used regularly, although it will affect readings
-    Serial.begin(9600);
-    ref = init_touchSwitch();
-    init_led_module();
+  // No pins to setup, pins can still be used regularly, although it will affect readings
+  Serial.begin(9600);
+  ref = init_touchSwitch();
+  init_led_module();
+  pinMode(ledPin, OUTPUT);
+  init_timer();
 } 
 
 
@@ -126,26 +210,48 @@ void loop()
     if(bts[0]==1)
     {
       Serial.println("buton1 is pressed");
-    }
-    if(bts[1]==1)
+      set_led_on(0);
+      pwm_duty1++;
+    }else
     {
-      Serial.println("buton2 is pressed");
+      set_led_off(0);
     }
-    if(bts[2]==1)
-    {
-      Serial.println("buton3 is pressed");
-    }
-    if(bts[3]==1)
-    {
-      Serial.println("buton4 is pressed");
-    }
-    if(bts[4]==1)
-    {
-      Serial.println("buton5 is pressed");
-    }
-    if(bts[5]==1)
-    {
-      Serial.println("buton6 is pressed");
-    }
-    delay(10);
+
+   if(bts[1]==1)
+   {
+     pwm_duty1--;
+     if (pwm_duty1 == 0)
+     {
+        pwm_duty1 = MAX_DUTY;
+     }
+     Serial.println("buton2 is pressed");
+   }
+   if(bts[2]==1)
+   {
+     Serial.println("buton3 is pressed");
+     if (pwm_duty2 < MAX_DUTY)
+     {
+        pwm_duty2++;
+        //pwm_duty2 = MAX_DUTY;
+     }
+   }
+   if(bts[3]==1)
+   {
+     Serial.println("buton4 is pressed");
+     
+     if (pwm_duty2 > 1)
+     {
+        pwm_duty2--;
+        //pwm_duty2 = MAX_DUTY;
+     }
+   }
+   if(bts[4]==1)
+   {
+     Serial.println("buton5 is pressed");
+   }
+   if(bts[5]==1)
+   {
+     Serial.println("buton6 is pressed");
+   }
+   
 }
